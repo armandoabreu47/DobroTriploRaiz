f49057e5f4b8a602672a5fba7cac31e0e0cbac6a
Armando Abreu <38231368+armandoabreu47@users.noreply.github.com>
2022-06-02 11:29:24 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+dobro = n * 2
+triplo = n * 3
+raiz = n ** (1/2)
+print('O dobro do número {} é {}'.format(n,dobro))
+print('O triplo do número {} é {}'.format(n,triplo))
+print('A raiz quadrada do número {} é {}'.format(n,raiz))
